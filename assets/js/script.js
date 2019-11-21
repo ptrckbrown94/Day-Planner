f7f5d54bc8de57353c3ca3502a52b935cff3ad78
Pat Brown <ptrckbrown94@gmail.com>
2019-11-21 15:05:26 -0600

    updated starting out

---
+++ assets/js/script.js
@@ -0,0 +1,55 @@
+console.log("hi")
+
+/*
+
+n result in missed meetings and deadlines or create the appearance of unprofessionalism. A daily planner allows employees to see their day at a glance, schedule time effectively, and improve productivity. 
+
+## Minimum Requirements
+
+* Functional, deployed application.
+
+* GitHub repository with a unique name and a README describing project.
+
+* The application displays timeblocks for standard business hours (9 a.m. to 5 p.m.).
+
+* Each timeblock contains an input field and save button.
+
+* Clicking a timeblock's "Save" button stores the input text in local storage, allowing the text to persist when the application is refreshed.
+
+* The current day is displayed at the top of the calendar.
+
+* Each timeblock is color coded to indicate whether it is in a past, present, or future hour.
+*/
+
+
+// function to display date and time at top of page
+function
+// get them element that the info goes into
+// decide on what it should look like
+// and then update it...
+// should this run in a loop and update the clock regularyary 
+
+// creat timeblocks with inputs and buttons
+// loop and dyamically
+// add event to submit button to save what is in the 
+//      input in local storage
+//  get the value out of local storarge and populate
+//      the input with teh datea
+// check to see what the color should be based on the time?
+//      moment?
+// look at css for classes later for this 
+
+// store data in local storage as an object where teh key is the hour of the day...
+
+/*
+{
+9:""
+10:""
+11:""
+12:""
+13:""
+14:""
+15:""
+16:""
+17:""
+}
