a48cc2886f1f11c49a1d9a6f8b356bb86d360358
Pat Brown <ptrckbrown94@gmail.com>
2019-11-21 23:58:22 -0600

    final

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,15 +22,22 @@ n result in missed meetings and deadlines or create the appearance of unprofessi
 */
 
 
-//global variable
-const currentDay = moment().format('MMMM Do YYYY, h:mm:ss a')
-let hours = ["9:00 am","10:00 am", "11:00 am", "12:00 pm", "1:00 pm", "2:00 pm", "3:00 pm", "4:00 pm", "5:00 pm" ]  
+//global variables
+const currentDay = moment().format('MMMM Do YYYY, h:mm:ss a');
 
-for()
+let currentHour = moment().format('h');
+
+const indexHourOffset = 9
+
+let hours = ["9:00 am", "10:00 am", "11:00 am", "12:00 pm", "1:00 pm", "2:00 pm", "3:00 pm", "4:00 pm", "5:00 pm"]
+
+//Temprary data holder
+let noteArray = []
+
 // function to display date and time at top of page
 
-function displayDate(){
-   const currentDayEl = document.getElementById("currentDayId");
+function displayDate() {
+    const currentDayEl = document.getElementById("currentDayId");
     currentDayEl.textContent = currentDay;
 }
 displayDate()
@@ -46,42 +53,103 @@ const timeBlock = document.createElement("table")
 // creat timeblocks with inputs and buttons
 // loop and dyamically
 
-function generatePlanner(){
+function generatePlanner() {
 
-//
-//<div class="row">
-//      <div class="col-2">
-//        <h4>9:00 am</h4>
-//      </div>
-//      <div class="col-8">
-//          <textarea name="message" rows="6" cols="50" class = "future">The cat was playing in the garden.</textarea>
-//      </div>
-//      <div class="col-2">
-//        <button class="saveBtn btn-lg">Save</button>
-//      </div>
-//    </div>
-//
+    //This is what the generated code needs to look like after testing in html
+    //
+    //<div class="row">
+    //      <div class="col-2">
+    //        <h4>9:00 am</h4>
+    //      </div>
+    //      <div class="col-8">
+    //          <textarea name="message" rows="6" cols="50" class = "future">The cat was playing in the garden.</textarea>
+    //      </div>
+    //      <div class="col-2">
+    //        <button class="saveBtn btn-lg">Save</button>
+    //      </div>
+    //    </div>
+    //
+
+    console.log("Current hour = " + currentHour)
+
+    const timeBlockDiv = document.getElementById("timeBlockId");
+
+    //use to generate questions
+    const rowDiv = document.createElement("div");
+
+    rowDiv.setAttribute("class", "row");
+    timeBlockDiv.appendChild(rowDiv);
+
+    for (i = 0; i < hours.length; i++) {
+        
+        const hoursDiv = document.createElement("div");
+        const toDoDiv = document.createElement("div");
+        const saveDiv = document.createElement("div");
+        const hoursH4 = document.createElement("h4");
+        const toDoText = document.createElement("textarea");
+        const saveButton = document.createElement("button");
+
+        hoursDiv.setAttribute("class", "col-2");
+        toDoDiv.setAttribute("class", "col-8");
+        saveDiv.setAttribute("class", "col-2");
+
+        hoursH4.innerText = hours[i];
+        hoursH4.id = i;
+
+        //Color the text box
+        //Known bug, I didn't have time to sort out AM/PM - it works from 9am to 12pm...
+        if(currentHour < i + indexHourOffset ){
+            toDoText.setAttribute("class", "past");
+        }
+        else if(currentHour == i + indexHourOffset) {
+            toDoText.setAttribute("class", "present");
+
+        } else if(currentHour > i + indexHourOffset) {
+            toDoText.setAttribute("class", "future");
+        }
+        
+                
+        toDoText.setAttribute("name", "message");
+        toDoText.setAttribute("col", "100");
+        toDoText.setAttribute("row", "6");
+
+
+        //Now get the data we saved if any
+        const toDoData = localStorage.getItem(i);
+        toDoText.value = toDoData;
+
+        saveButton.setAttribute("class", "savebtn");
+        saveButton.setAttribute("class", "btn-lg");
+        saveButton.innerText = "Save";
+
+        toDoText.addEventListener("change", function(){
+            saveNoteTemporary(hoursH4.id, toDoText.value);
+        });
+
+        saveButton.addEventListener("click", function(){
+            saveNote(hoursH4.id);
+        });
+
+        //Load note array from storage
+        hoursDiv.appendChild(hoursH4);
+        toDoDiv.appendChild(toDoText);
+        saveDiv.appendChild(saveButton);
+
+        rowDiv.appendChild(hoursDiv);
+        rowDiv.appendChild(toDoDiv);
+        rowDiv.appendChild(saveDiv);
+    }
+
 }
-// add event to submit button to save what is in the 
-//      input in local storage
-//  get the value out of local storarge and populate
-//      the input with the data
-// check to see what the color should be based on the time?
-//      moment?
-// look at css for classes later for this 
 
-// store data in local storage as an object where the key is the hour of the day...
-
-/*
-{
-9:""
-10:""
-11:""
-12:""
-13:""
-14:""
-15:""
-16:""
-17:""
+function saveNote(index){
+    console.log("saveNote index=" + index)
+    localStorage.setItem(index, JSON.stringify(noteArray[index]));
 }
-*/
+
+function saveNoteTemporary(index, note) {
+    
+    console.log("saveNoteTemporary index=" + index + " note=" + note)
+    noteArray[index] = note;
+}
+
